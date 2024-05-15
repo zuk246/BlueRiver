4128d5931f1c45cf9d57b8ae3f7c1c3a3971b54f
zuk246 <tadasukeiizuka@gmail.com>
2024-05-15 11:31:49 +0900

    add: function to get relative time

---
+++ src/features/getRelativeTime.ts
@@ -0,0 +1,23 @@
+import dayjs from 'dayjs';
+
+export default function getRelativeTime(date: string) {
+    let relativeTime = -1 * dayjs(date).diff(dayjs(), 'minute');
+
+    if (relativeTime == 0) {
+        return 'now';
+    }
+
+    if (relativeTime < 60) {
+        return `${relativeTime}m`;
+    }
+
+    if (relativeTime < 24 * 60) {
+        return `${Math.floor(relativeTime / 60)}h`;
+    }
+
+    if (relativeTime < 24 * 60 * 7) {
+        return `${Math.floor(relativeTime / 60 / 24)}d`;
+    }
+
+    return `${dayjs(date).format('YYYY/MM/DD')}`;
+}
