f83bd88e65ee22322454c8a71a9e102cb330fcdd
zuk246 <tadasukeiizuka@gmail.com>
2024-05-15 21:58:18 +0900

    add: command list command

---
+++ src/command/list.ts
@@ -0,0 +1,49 @@
+import * as vscode from 'vscode';
+
+interface CommandItem extends vscode.QuickPickItem {
+    command: string;
+    secondaryCommand?: string;
+}
+
+const items: CommandItem[] = [
+    {
+        label: 'Timeline',
+        description: 'Show the timeline',
+        command: 'blueriver.timeline',
+        iconPath: new vscode.ThemeIcon('comment-discussion'),
+    },
+    {
+        label: 'Post',
+        description: 'Create a new post',
+        command: 'blueriver.post',
+        iconPath: new vscode.ThemeIcon('edit'),
+    },
+    {
+        label: 'Notifications',
+        description: 'Show the notifications',
+        command: 'blueriver.notifications',
+        iconPath: new vscode.ThemeIcon('bell'),
+    },
+    {
+        kind: vscode.QuickPickItemKind.Separator,
+        command: '',
+        label: '',
+    },
+    {
+        label: 'Settings',
+        description: 'Show the settings',
+        command: 'workbench.action.openSettings',
+        secondaryCommand: 'blueriver',
+        iconPath: new vscode.ThemeIcon('gear'),
+    },
+];
+
+export default async function listCommand() {
+    const command = await vscode.window.showQuickPick(items);
+
+    if (!command) {
+        return;
+    }
+
+    vscode.commands.executeCommand(command.command, command.secondaryCommand);
+}
