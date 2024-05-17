61c197dda3cff83557973096dd19a3b368bc23f8
zuk246 <tadasukeiizuka@gmail.com>
2024-05-17 20:27:27 +0900

    feat: Add notifications_text constant for notification messages

---
+++ src/data/notifications.ts
@@ -0,0 +1,8 @@
+export const notifications_text = {
+    reply: 'replied to your post',
+    repost: 'reposted your post',
+    like: 'liked your post',
+    follow: 'followed you',
+    mention: 'mentioned you',
+    quote: 'quoted your post',
+};
