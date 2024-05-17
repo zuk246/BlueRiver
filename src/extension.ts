d1d97d8fa26abda0b21898d4357c76b9b287aa4a
zuk246 <tadasukeiizuka@gmail.com>
2024-05-17 20:45:45 +0900

    refactor: Update extension configuration key to 'blueriver'

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,45 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { NotificationsView, TimelineView } from './webview';
+import showStatusBar from './status_bar';
+import { listCommand, postCommand } from './command';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    // status bar
+    showStatusBar();
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-to-bluesky" is now active!');
+    // list
+    vscode.commands.registerCommand('blueriver.list', async () => {
+        await listCommand();
+    });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-to-bluesky.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-to-bluesky!');
-	});
+    // timeline
+    vscode.commands.registerCommand('blueriver.timeline', () => {
+        const panel = vscode.window.createWebviewPanel(
+            'timeline',
+            'Bluesky Timeline',
+            vscode.ViewColumn.One,
+            {}
+        );
+        const view = new TimelineView(context.extensionUri);
+        view.resolveWebviewView(panel);
+    });
 
-	context.subscriptions.push(disposable);
+    // post
+    vscode.commands.registerCommand('blueriver.post', async () => {
+        await postCommand();
+    });
+
+    // notifications
+    vscode.commands.registerCommand('blueriver.notifications', () => {
+        const panel = vscode.window.createWebviewPanel(
+            'notifications',
+            'Bluesky Notifications',
+            vscode.ViewColumn.One,
+            {}
+        );
+        const view = new NotificationsView(context.extensionUri);
+        view.resolveWebviewView(panel);
+    });
 }
 
 // This method is called when your extension is deactivated
