10da5413b235caf5585cb2945b2267fce6434fd0
zuk246 <tadasukeiizuka@gmail.com>
2024-05-19 15:07:29 +0900

    add: continuous updates to status bar

--- src/status_bar/index.ts
+++ src/status_bar/index.ts
@@ -1,22 +1,33 @@
 import * as vscode from 'vscode';
 import Bluesky from '../bluesky';
 
-export default async function showStatusBar() {
-    const bluesky = new Bluesky();
-    await bluesky.login();
-
-    const unreadNotifications = await bluesky.notificationCount();
-
+export default function showStatusBar() {
     const status_bar = vscode.window.createStatusBarItem(
         vscode.StatusBarAlignment.Right,
-        1000
+        2
     );
 
-    status_bar.text =
-        unreadNotifications > 0
-            ? `$(bluesky-logo) ${unreadNotifications} unread`
-            : '$(bluesky-logo)';
+    status_bar.text = '$(bluesky-logo)';
+    status_bar.tooltip = 'Blueriver';
+    status_bar.name = 'Blueriver';
     status_bar.command = 'blueriver.list';
 
     status_bar.show();
+
+    const bluesky = new Bluesky();
+
+    setInterval(async () => {
+        status_bar.show();
+
+        await bluesky.login();
+
+        const unreadNotifications = await bluesky.notificationCount();
+
+        status_bar.text =
+            unreadNotifications > 0
+                ? `$(bluesky-logo) ${unreadNotifications} unread`
+                : '$(bluesky-logo)';
+
+        status_bar.show();
+    }, 30000);
 }
